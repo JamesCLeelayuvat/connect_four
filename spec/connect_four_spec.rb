ffc95b00f24b5c8c15a4cc591ce2bd78d4eaca68
James C Leelayuvat <jmsleelayuvat@gmail.com>
2022-05-04 09:10:18 +0700

    added tests

---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,52 @@
+require "./lib/connect_four"
+describe Connect_four do
+  describe "#create_board" do
+    it "creates a new 4x4 board of nested arrays" do
+      connect_four = Connect_four.new()
+      expect(connect_four.create_board).to eql([[nil, nil, nil, nil, nil, nil],
+                                                [nil, nil, nil, nil, nil, nil],
+                                                [nil, nil, nil, nil, nil, nil],
+                                                [nil, nil, nil, nil, nil, nil],
+                                                [nil, nil, nil, nil, nil, nil],
+                                                [nil, nil, nil, nil, nil, nil],
+                                                [nil, nil, nil, nil, nil, nil]])
+    end
+  end
+  describe "#show_board" do
+    it "shows the board" do
+      connect_four = Connect_four.new()
+      connect_four.create_board
+      expect(connect_four.show_board).to eql("⛒⛒⛒⛒⛒⛒⛒\n⛒⛒⛒⛒⛒⛒⛒\n⛒⛒⛒⛒⛒⛒⛒\n⛒⛒⛒⛒⛒⛒⛒\n⛒⛒⛒⛒⛒⛒⛒\n⛒⛒⛒⛒⛒⛒⛒")
+    end
+  end
+  describe "#add_token" do
+    it "adds a token on the board for player 1" do
+      connect_four = Connect_four.new()
+      expect(connect_four.add_token(0, 1)).to eql([[0, nil, nil, nil, nil, nil],
+                                                   [nil, nil, nil, nil, nil, nil],
+                                                   [nil, nil, nil, nil, nil, nil],
+                                                   [nil, nil, nil, nil, nil, nil],
+                                                   [nil, nil, nil, nil, nil, nil],
+                                                   [nil, nil, nil, nil, nil, nil],
+                                                   [nil, nil, nil, nil, nil, nil]])
+    end
+    it "adds a token on the board for player 2" do
+      connect_four = Connect_four.new()
+      expect(connect_four.add_token(0, 2)).to eql([[1, nil, nil, nil, nil, nil],
+                                                   [nil, nil, nil, nil, nil, nil],
+                                                   [nil, nil, nil, nil, nil, nil],
+                                                   [nil, nil, nil, nil, nil, nil],
+                                                   [nil, nil, nil, nil, nil, nil],
+                                                   [nil, nil, nil, nil, nil, nil],
+                                                   [nil, nil, nil, nil, nil, nil]])
+    end
+  end
+  describe "#check_win"
+    it "checks if the player has won based on the last token placed" do
+      connect_four = Connect_four.new()
+      connect_four.add_token(1,1)
+      connect_four.add_token(2,1)
+      connect_four.add_token(3,1)
+      expect(connect_four.check_win(0)).to eql(true)
+    end
+end
